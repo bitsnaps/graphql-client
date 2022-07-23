4b1523302c217a675f1a53b2d03a32d65e4604f2
Ibrahim.H <bitsnaps@yahoo.fr>
2022-07-23 18:12:26 +0100

    simple gql example

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,43 @@
 <template>
-  <div></div>
+  <div>
+    <p v-for="book in result.allBooks" :key="book.id">
+    {{ book.title }}
+    </p>
+  </div>
 </template>
 
 <script>
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+const ALL_BOOKS_QUERY = gql`
+    query allBooks {
+        allBooks {
+            id
+            title
+            rating
+        }
+    }
+`
+/*
+apolloClient
+    .query({
+        query: ALL_BOOKS_QUERY
+    })
+    .then( res => {
+        console.log(res)
+    })
+*/
+export default {
+    name: 'App',
+    // This will be executed before the creation of the component
+    setup() {
+      const { result } = useQuery(ALL_BOOKS_QUERY)
+      // console.log(result);
+      // return results to make it available to the template
+      return { result }
+    }
+  }
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,53 @@
-import { createApp } from 'vue'
+import { createApp, h, provide } from 'vue'
+import { 
+    ApolloClient, 
+    createHttpLink, 
+    InMemoryCache 
+} from '@apollo/client/core'
 import App from './App.vue'
+import gql from 'graphql-tag'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
-createApp(App).mount('#app')
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/graphql'
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache
+})
+
+/*
+const ALL_BOOKS_QUERY = gql`
+    query allBooks {
+        allBooks {
+            id
+            title
+            rating
+        }
+    }
+`
+
+apolloClient
+    .query({
+        query: ALL_BOOKS_QUERY
+    })
+    .then( res => {
+        console.log(res)
+    })
+*/
+
+// Need some refactoring...
+//createApp(App).mount('#app')
+
+// With that we can make calls using Apollo Client form vue's components
+const app = createApp({
+    setup() {
+        provide(DefaultApolloClient, apolloClient)
+    },
+    render: () => h(App),
+})
+
+app.mount('#app')
